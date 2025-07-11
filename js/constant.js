c805ebb14b8d55999bc74c5ac0ffb30d544d726f
Ryoo-code <ryotanakamura0310@gmail.com>
2025-07-11 23:21:47 +0900

    整数,変数,定数の扱い

---
+++ js/constant.js
@@ -0,0 +1,8 @@
+// 定数の宣言・値の代入
+const shippingFee = 500;
+//コンソールの出力
+console.log(shippingFee);
+
+//値の再代入
+shippingFee = 300;
+console.log(shippingFee);
---
+++ js/veriable.js
@@ -0,0 +1,20 @@
+//変数の宣言 let
+let userName;
+
+//値の代入
+userName = '侍太郎';
+
+//変数の宣言・値の代入
+let userNumber = 55;
+
+//コンソールの出力
+console.log(userName);
+console.log(userNumber);
+
+//値の再代入
+userName = '侍花子';
+userNumber = 66;
+
+//コンソールの出力
+console.log(userName);
+console.log(userNumber);
